d87140ac1d38a954fb57832fb6480de176eb5c53
angelakumaresan-arch <angela.kumaresan@students.asdrp.org>
2025-10-12 16:47:03 -0700

    Update angela.py

--- angela.py
+++ angela.py
@@ -1,8 +1,47 @@
 import random 
-word_bank = ["songwriting", "music", "beyond", "words", "wildcat", "review"]
+word_bank = ["musim"]
 random_value = random.choice(word_bank)
+
 character_count = len(random_value)
-question = ("_ ") * character_count
-print(question)
+guessing_reference = list(random_value)
+
+question = ("_") * character_count
+joined_string = " ".join(question)
+print(joined_string)
+rounds_played = 0
+
 lives_remaining = str(6)
 print("Lives remaining: " + lives_remaining)
+lives_remaining = int(lives_remaining)
+
+
+
+while lives_remaining > 0:
+    user_guess = input("Guess a letter: ")
+    user_guess_length = len(user_guess)
+    while(user_guess_length > 1):
+        print("Invalid. Try again")
+        user_guess = input("Guess a letter: ")
+        user_guess_length = len(user_guess)
+        if (user_guess_length == 1):
+            break
+    if rounds_played == character_count + 6:
+        break
+    if user_guess in guessing_reference:
+        guess_number = guessing_reference.index(user_guess)
+        question = list(question)
+        question[guess_number] = user_guess
+        joined_string = " ".join(question)
+        print(joined_string)
+        rounds_played = rounds_played + 1
+        continue
+    else:
+        print("Letter not found!")
+        lives_remaining = lives_remaining - 1
+        print("Lives remaining: " + str(lives_remaining))
+        rounds_played = rounds_played + 1
+        if lives_remaining = 0
+            print("you are a failure my dude")
+        continue
+
+print("slayy")
